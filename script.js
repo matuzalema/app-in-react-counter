1119868786ab41678ad1a3637d5243d66865269e
Jadwiga Bochinska <matuzalema@gmail.com>
2019-02-20 10:11:25 +0100

    Change spacing

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
 var Counter = React.createClass({
-	getDefaultProps: function(){
-		console.log('ustawianie domyślnych propsów jeśli nie są przekazane do komponentu');
+    getDefaultProps: function(){
+        console.log('ustawianie domyślnych propsów jeśli nie są przekazane do komponentu');
 	},
 
     getInitialState: function() {
-    	console.log('początkowy stan komponentu');
+        console.log('początkowy stan komponentu');
         return {
             counter: 0
         };
     },
 
-	increment: function(){
-		this.setState({
-			counter: this.state.counter + 1
-		});
+    increment: function(){
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    decrement: function(){
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+
+    componentWillMount: function(){
+        console.log('bardzo rzadko jest używana');
 	},
 
-	decrement: function(){
-		this.setState({
-			counter: this.state.counter - 1
-		});
-	},
-
-	componentWillMount: function(){
-		console.log('bardzo rzadko jest używana');
-	},
-
-	render: function(){
+    render: function(){
         return (
-        	React.createElement('div', {'className': 'counter'},
-        		React.createElement('h2', {}, 'licznik: ' + this.state.counter),
+            React.createElement('div', {'className': 'counter'},
+                React.createElement('h2', {}, 'licznik: ' + this.state.counter),
         		React.createElement('button', {onClick: this.increment}, 'dodaj'),
         		React.createElement('button', {onClick: this.decrement}, 'odejmij')
         	)
         );
 	},
 
-	componentDidMount: function(){
+    componentDidMount: function(){
 		console.log('aktualizacja stanu komponentu, wywołanie AJAX, pobieranie danych, użycie jQuery');
 	},
 
-	componentWillReceiveProps: function(){
+    componentWillReceiveProps: function(){
         console.log('np sprawdzenie, czy odświeżenie nastąpiło w wyniku zmiany obiektu this.props (a props jeszcze nie jest zaktualizowany, wywołanie this.setState()');
 	},
-	
-	shouldComponentUpdate: function(){
-	 	console.log('true/false, np sprawdzenie, czy nowy this.props jest taki sam jak stary, jeśli tak to zwróci false i kolejne metody cyklu beda anulowanie');
+
+    shouldComponentUpdate: function(){
+        console.log('true/false, np sprawdzenie, czy nowy this.props jest taki sam jak stary, jeśli tak to zwróci false i kolejne metody cyklu beda anulowanie');
         return true;
 	},
 
-	componentWillUpdate: function(){
-		console.log('jeśli shouldComponentUpdate zwróci true to odpali sie używa sie np do animacji i przygotowania zmian w DOM');
+    componentWillUpdate: function(){
+        console.log('jeśli shouldComponentUpdate zwróci true to odpali sie używa sie np do animacji i przygotowania zmian w DOM');
 	},
 	
 	// renderuje
 
 	componentDidUpdate: function(){
-		console.log('po zmianach, teraz możliwa jest zmiana DOM');
+        console.log('po zmianach, teraz możliwa jest zmiana DOM');
 	},
 
 	componentWillUnmount: function(){
-		console.log('czyszczenie komponentu np. anulowanie subskrypcji, żądań serwera, usunicie nasłuchiwania zdarzeń');
-	}
+        console.log('czyszczenie komponentu np. anulowanie subskrypcji, żądań serwera, usunicie nasłuchiwania zdarzeń');
+    }
 });
 
 var element = React.createElement('div', {}, 
